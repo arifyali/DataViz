0fd930d18fdcc9492e873bdeaaeaaaef8057b8f7
Arif <arifyuali@gmail.com>
2016-10-06 19:00:21 -0400

    First week of DataViz

---
+++ Plots.R
@@ -0,0 +1,29 @@
+U.S._Chronic_Disease_Indicators__CDI_ <- read.csv("~/Downloads/U.S._Chronic_Disease_Indicators__CDI_.csv")
+Readmissions_and_Deaths_._Hospital <- read.csv("~/Downloads/Readmissions_and_Deaths_-_Hospital.csv")
+Medicare_Hospital_Spending_by_Claim <- read.csv("~/Downloads/Medicare_Hospital_Spending_by_Claim.csv")
+
+### First Plot
+library("sqldf")
+Readmissions = Readmissions_and_Deaths_._Hospital[grepl("READM", Readmissions_and_Deaths_._Hospital$Measure.ID), ]
+names(Readmissions) = gsub("\\.","_", names(Readmissions))
+Readmissions_by_state = sqldf("SELECT AVG(Score) AS average_score, State, Measure_Name AS readmission_type FROM Readmissions
+                               GROUP BY State, Measure_Name;")
+Readmissions_by_state = Readmissions_by_state[order(Readmissions_by_state$average_score), ]
+Readmissions_by_state$readmission_type = gsub("Rate of readmission for ", "",Readmissions_by_state$readmission_type)
+Readmissions_by_state$readmission_type = gsub("Rate of readmission after ", "",Readmissions_by_state$readmission_type)
+Readmissions_by_state$readmission_type = gsub(" \\(.*", "",Readmissions_by_state$readmission_type)
+
+Readmissions_by_state$readmission_type = gsub(" Readmission Rate", "",Readmissions_by_state$readmission_type)
+Readmission_types = unique(Readmissions_by_state$readmission_type)[-6]
+par(mfrow =c(3,3),oma=c(0,0,2,0))
+for(i in Readmission_types){
+  Readmissions_type = merge(Readmissions_by_state[Readmissions_by_state$readmission_type == i,], Readmissions_by_state[Readmissions_by_state$readmission_type == "discharge from hospital",], by.x = "State", by.y = "State")
+  Readmissions_type = Readmissions_type[order(Readmissions_type$average_score.y, decreasing = T)[1:10],]
+  print(Readmissions_type$average_score.y)
+  barplot(t(as.matrix(Readmissions_type[,c("average_score.y")])), names.arg=Readmissions_type$State,col = c("grey80"), border=NA, space=0.25, xlab="State", main = toupper(i),ylab = "Rate of Readmin. (%)", ylim = c(0, 25))
+  barplot(t(as.matrix(Readmissions_type[,c("average_score.x")])), names.arg=Readmissions_type$State,col = c("#FF0000FF"), border=NA, space=0.25, xlab="State", ylim = c(0, 25), add = T)
+}
+plot(1, type="n", axes=F, xlab="", ylab="")
+rect(par("usr")[1],par("usr")[3],par("usr")[2],par("usr")[4],col = "grey80")
+legend("right", legend = c("Rate of Overall readmission"), bty = "n", cex=0.75)
+title("The rates of Readmission by Reason for the Top 10 Highest Readmitting States (averaged by hospital)", outer = T)
