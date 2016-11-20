cc49b8d4ad37ed4427fb5469b3e5f564dd6564e0
Arif Ali <arifyuali@gmail.com>
2016-11-20 14:04:00 -0500

    code for graphs 10 and 11

--- Plots.R
+++ Plots.R
@@ -1,4 +1,4 @@
-setwd("~/Documents/Georgetown/Data-Visualization-PPOL-646-Fall-2016/")
+setwd("~/Documents/DataViz/")
 U.S._Chronic_Disease_Indicators__CDI_ <- read.csv("U.S._Chronic_Disease_Indicators__CDI_.csv")
 Readmissions_and_Deaths_._Hospital <- read.csv("Readmissions_and_Deaths_-_Hospital.csv")
 Medicare_Hospital_Spending_by_Claim <- read.csv("Medicare_Hospital_Spending_by_Claim.csv")
@@ -39,6 +39,7 @@ title("The rates of Readmission by Reason for the Top 10 Highest Readmitting Sta
 
 
 ### Plot 2
+par(mfrow =c(1,1))
 Patient_survey__HCAHPS__._Hospital <- read.csv("Patient_survey__HCAHPS__-_Hospital.csv")
 View(Patient_survey__HCAHPS__._Hospital)
 Patient_survey__HCAHPS__._Hospital$Patient.Survey.Star.Rating = as.numeric(as.character(Patient_survey__HCAHPS__._Hospital$Patient.Survey.Star.Rating))
@@ -59,16 +60,17 @@ merged_states = sqldf("SELECT * FROM clean_avg_state
 merged_states = na.omit(merged_states)
 merged_states = merged_states[order(merged_states$avg_clean_rating, decreasing = T)[1:20],]
 
-symbols(x = c(merged_states$avg_clean_rating,rep(5, 5)), 
-        y = c(merged_states$average_score,rep(3.6,5)), 
-        circles = sqrt(c(merged_states$total_readmission, c(1e5,2.5e5,5e5,7.5e5, 1e6)/1e6)),
-        bg = "#FF000022", ylim = c(0,20))
+symbols(x = merged_states$avg_clean_rating, 
+        y = merged_states$average_score, 
+        circles = sqrt(merged_states$total_readmission),
+        bg = "#FF000022")
 
 text(x = c(merged_states$avg_clean_rating), y = merged_states$average_score, labels = merged_states$State)
 
-symbols(x = rep(5, 5), y = rep(3.6,5), circle = sqrt(10^(0:4)), add = T)
 
 ### Plot 3
+par(mfrow =c(1,1))
+
 Age.adjusted_death_rates_1900_2013 <- read.csv("NCHS_-_Age-adjusted_death_rates_and_life-expectancy_at_birth___All_Races__Both_Sexes___United_States__1900-2013.csv",  stringsAsFactors=FALSE)
 Age.adjusted_death_rates_1900_2013 = Age.adjusted_death_rates_1900_2013[!is.na(Age.adjusted_death_rates_1900_2013$Average.Life.Expectancy), ]
 par(mfrow = c(3,1))
@@ -227,3 +229,45 @@ p <- p + geom_polygon(data=Total, aes(x=long, y=lat, group = group, fill=Total$D
 P1 <- p + theme_bw()  + labs(fill = "Cancer-related Deaths \n per 100,000 people" 
                              ,title = "Cancer Motality in the United States (2008-2012)", x="", y="")
 P1 + scale_y_continuous(breaks=c()) + scale_x_continuous(breaks=c()) + theme(panel.border =  element_blank())
+
+### Plot 10
+library(rbokeh)
+READM_30_HOSP_WIDE = Readmissions_and_Deaths_._Hospital[Readmissions_and_Deaths_._Hospital$Measure.ID == "READM_30_HOSP_WIDE", c("Provider.ID", "Score","Denominator", "Compared.to.National", "Hospital.Name","City","State")]
+names(READM_30_HOSP_WIDE) = c("Provider_ID", "Readmission_Score","Denominator", "Compared.to.National","Hospital.Name","City","State")
+READM_30_HOSP_WIDE$Readmission_Score = as.numeric(as.character(READM_30_HOSP_WIDE$Readmission_Score))
+READM_30_HOSP_WIDE = READM_30_HOSP_WIDE[!is.na(READM_30_HOSP_WIDE$Readmission_Score),]
+
+hist(READM_30_HOSP_WIDE$Readmission_Score, breaks = 100, freq = FALSE, col = "grey", main = "Histogram of U.S. Hospital Readmission Scores (closely follows a Normal Distribution with a left tail)", xlab = "Readmission Score")
+lines(density(READM_30_HOSP_WIDE$Readmission_Score))
+abline(v = median(READM_30_HOSP_WIDE$Readmission_Score), col ="#FF000099", lty="dotted", lwd = 4)
+
+### Plot 11
+par(mfrow = c(1,1))
+tobacco <- U.S._Chronic_Disease_Indicators__CDI_[U.S._Chronic_Disease_Indicators__CDI_$Topic == "Tobacco", ]
+
+youth_tobacco_smoke = tobacco[tobacco$Question == "Current cigarette smoking among youth",c("LocationDesc","DataValue")]
+names(youth_tobacco_smoke) = c("State", "Percent_Youth_Smoke")
+
+youth_tobacco_smokeless = tobacco[tobacco$Question == "Current smokeless tobacco use among youth",c("LocationDesc","DataValue")]
+names(youth_tobacco_smokeless) = c("State", "Percent_Youth_tobacco_(smokeless)")
+
+License_requirement = tobacco[tobacco$Question == "States with strong polices that require retail licenses to sell tobacco products",c("LocationDesc","DataValue")]
+names(License_requirement) = c("State", "License_requirement")
+
+youth_tobacco = sqldf("SELECT * FROM youth_tobacco_smoke
+                      JOIN youth_tobacco_smokeless
+                      ON youth_tobacco_smoke.State = youth_tobacco_smokeless.State
+                      JOIN License_requirement
+                      ON youth_tobacco_smokeless.State = License_requirement.State")
+youth_tobacco$Percent_Youth_Smoke = as.numeric(as.character(youth_tobacco$Percent_Youth_Smoke))
+youth_tobacco$`Percent_Youth_tobacco_(smokeless)` = as.numeric(as.character(youth_tobacco$`Percent_Youth_tobacco_(smokeless)`))
+
+youth_tobacco_license = youth_tobacco[youth_tobacco$License_requirement == "Yes",]
+
+ggplot(data = youth_tobacco, aes(x = Percent_Youth_Smoke, y = `Percent_Youth_tobacco_(smokeless)`)) + 
+  geom_point(col = "grey") +
+  geom_point(data = youth_tobacco_license, aes(x = Percent_Youth_Smoke, y = `Percent_Youth_tobacco_(smokeless)`), col = "#FF000099") +
+  ggtitle("Requiring Licenses to Sell Cigerattes or other Tobacco Products does not affect Youth Tobacco Use") +
+  xlab("Percent of Youth that are smoking") + ylab("Percent of Youth that are using smokeless tobacco products")
+
+
